b960818d02c419d0a88ce333396c6acced871d5f
Jocapear <jose18_carl@hotmail.com>
2018-10-23 11:18:56 -0500

    Made corrections. Demo version to host on GCloud

--- src/models/users.js
+++
@@ -1,7 +0,0 @@
-const User = database.define('users', {
-  partNumber: Sequelize.STRING,
-  modelNumber: Sequelize.STRING,
-  name: Sequelize.STRING,
-  description: Sequelize.TEXT
-})
-moduel.exports = User
