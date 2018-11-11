a4fa13b4cd6f5e4153f866e54b44434dde071b70
Andres Barro <andresbarroe+git@gmail.com>
2018-11-11 14:23:22 -0600

    vote status

--- db/queries.sql
+++ db/queries.sql
@@ -5,16 +5,18 @@ FROM
         SELECT
             *,
             (
-                SELECT array_to_json(array_agg(row_to_json(poll)))
-                FROM poll JOIN (
-                    SELECT *
-                    FROM participation
-                    WHERE wiki_user_id = 1
-                ) p ON poll.id = p.poll_id
+                SELECT array_to_json(array_agg(row_to_json(q)))
+                FROM
+                    (
+                        SELECT poll.*, p.vote_status
+                        FROM poll JOIN (
+                            SELECT *
+                            FROM participation
+                            WHERE wiki_user_id = 1
+                        ) p ON poll.id = p.poll_id
+                    ) q
             ) AS questions
         FROM wiki_user
         WHERE id = 1
     ) t
 ;
-
-
