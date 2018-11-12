578c15229a06876781d1f0262b38eca926ec02d1
José Carlos Peñuelas Armenta <jose18carl@gmail.com>
2018-11-12 17:54:35 -0600

    Refactor/user queries (#9)
    * Authorization added to routs
    
    * Bug fix in routing

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,8 @@ const queries = require('../services/users.js');
 
 router.get('/', queries.getUsers);
 router.get('/:id', queries.getUserById);
-router.post('/',queries.createUser);
+router.post('/', queries.createUser);
 router.put('/:id', queries.updateUser);
 router.put('/:id', queries.deleteUser);
 
-module.exports = router
+module.exports = router;
