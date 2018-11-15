21142503b483c052994cd12ec769594115efe49e
geraJuarez <geratrex781@gmail.com>
2018-11-14 21:13:27 -0600

    added helper functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,11 @@
 const isEmptyObject = obj => (Object.getOwnPropertyNames(obj).length === 0);
 
+const isEmptyArray = array => array.length === 0;
+
+const isPositiveInteger = str => /^\+?(0|[1-9]\d*)$/.test(str);
+
 module.exports = {
   isEmptyObject,
+  isEmptyArray,
+  isPositiveInteger,
 };
