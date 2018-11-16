4d2fb26a993ac2c366933ac427ba275bef8aa708
geraJuarez <geratrex781@gmail.com>
2018-11-15 19:31:10 -0600

    Add constants of http errors

---
+++ src/constants/httpCodes.js
@@ -0,0 +1,16 @@
+const STATUS = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  INT_SERV_ERR: 500,
+};
+
+const MSG = {
+  INT_SERV_ERR: 'Error conecting to DB',
+};
+
+module.exports = {
+  STATUS,
+  MSG,
+};
