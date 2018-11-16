8160c05e5601d6fd789b34007086b14e172bcea8
geraJuarez <geratrex781@gmail.com>
2018-11-15 21:20:29 -0600

    Create the logger module

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config();
 const express = require('express');
+
 // call express
 const app = express(); // define our app using express
 const bodyParser = require('body-parser');
