a29b9a8c653712d3e6c8aea0d173254f7f1f5441
José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>
2018-11-18 17:58:49 -0600

    Mocha installed and basic test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var database = require('./database.js');
+describe('Connection', function() {
+    const all = database.
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1,2,3].indexOf(4), -1);
+        });
+    });
+});
