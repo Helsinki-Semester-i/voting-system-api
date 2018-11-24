1e20c5bc953f7094e39c6a71fc64c296ea2e5543
geraJuarez <geratrex781@gmail.com>
2018-11-23 20:51:52 -0600

    Refactor

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -1,15 +1,8 @@
 const resultsService = require('../services/results.js');
 
-const Log = require('../utils/logger');
-const Error = require('../errors/statusError');
-const utils = require('../utils/utils.js');
-const CODES = require('../constants/httpCodes');
-
-function throwErrorForQueryParams(queryParams) {
-  if (!utils.isEmptyObject(queryParams)) {
-    throw new Error(CODES.STATUS.BAD_REQUEST, 'Query params are not supported yet');
-  }
-}
+const {
+  Log, CODES, utils, throwErrorForQueryParams, Error,
+} = require('./validationUtils');
 
 const getResultById = async (req, res) => {
   try {
